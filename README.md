efab294f3d020ccb8b7d5649d01477bc14e327bd
datapsycho <mr.data.psycho@gmail.com>
2023-10-10 16:13:18 +0200

    Adding homework 2

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Rust Developer Project
 This repository represent homeworks from a rust inclusive course.
+Homeworks:
+    - 2: https://robot-dreams-rust.mag.wiki/2-rust-basics/index.html#homework
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use slug::slugify;
+use std::env;
+
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        let example_txt = "cargo run --lowercase/-l or --no-space/-ns or --slugify/-s";
+        panic!("Please provide a transformation flag to work with the text. Example: {}", example_txt);
+    }
+
+    println!("Enter a text:");
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+
+    let flag = &args[1];
+    let flag = flag.as_str();
+
+    match flag{
+        "--lowercase" | "-l" => {
+            println!("Lowercased: {}", input.to_lowercase());
+        },
+        "--uppercase" | "-u" => {
+            println!("Uppercased: {}", input.to_uppercase());
+        },
+        "--no-spaces" | "-ns" => {
+            println!("No spaced: {}", input.replace(" ", ""));
+        },
+        "--slugify" | "-s" => {
+            println!("Slugified: {}", slugify(&input));
+        }
+        _ => {
+            let flag_list = "--lowercase/-l, --uppercase/-u, --no-spaces/-ns, --slugify/-s";
+            panic!("Transformation flag {} is not exceptable flag! Available flags are: {}", flag, flag_list);
+        },
+        
+    }
+
 }
