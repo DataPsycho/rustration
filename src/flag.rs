c0b73c8f530389b5dfa91b94860714ce5b19bb4c
MrDataPsycho <datapsycho@MrDataPsychos-MacBook-Air.local>
2023-10-23 04:03:25 +0200

    Adding multiline features Committer: MrDataPsycho mr.data.psycho@gmail.com

--- src/flag.rs
+++ src/flag.rs
@@ -14,7 +14,7 @@ pub fn get_flag(flag: &str) -> Result<Flag, String> {
         "--slugify" | "-s" => Ok(Flag::Slugify),
         _ => {
             let available_flags =
-                "cargo run --lowercase/-l or --uppercase/-u or --no-space/-ns or --slugify/-s";
+                "--lowercase/-l or --uppercase/-u or --no-space/-ns or --slugify/-s";
             let err_text = format!(
                 "{} is not a valid flag. Available flags are: {}",
                 flag, available_flags
@@ -27,7 +27,7 @@ pub fn get_flag(flag: &str) -> Result<Flag, String> {
 pub fn parse_flag(args: Vec<String>) -> Result<Flag, String> {
     if args.len() < 2 {
         let example_txt =
-            "cargo run --lowercase/-l or --uppercase/-u or --no-space/-ns or --slugify/-s";
+            "--lowercase/-l or --uppercase/-u or --no-space/-ns or --slugify/-s";
         let err_text = format!(
             "Please provide a transformation flag to work with the text. Example: {}",
             example_txt
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,52 @@
 mod flag;
 mod transform;
 use std::env;
+use std::error::Error;
+use std::io::{self, BufRead};
 
-fn receive_input_from_user() -> String {
-    eprintln!("Enter a text:");
+// fn receive_input_from_user() -> String {
+//     eprintln!("Enter a text:");
+//     let mut input = String::new();
+//     std::io::stdin().read_line(&mut input).unwrap();
+//     input.trim().to_string()
+// }
+
+fn receive_multiline_input_from_user() -> Result<String, Box<dyn Error>> {
+    let mut lines = io::stdin().lock().lines();
     let mut input = String::new();
-    std::io::stdin().read_line(&mut input).unwrap();
-    input
+    while let Some(line) = lines.next() {
+        match line {
+            Ok(l) => {
+                if l.len() == 0 {
+                    break;
+                }
+                if input.len() > 0 {
+                    input.push_str("\n");
+                }
+                input.push_str(&l);
+            }
+            Err(e) => return Err(Box::new(e)),
+        }
+    }
+    let input = input.trim().to_string();
+    Ok(input)
 }
-
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let flag = flag::parse_flag(args).unwrap();
+    let flag = flag::parse_flag(args).unwrap_or_else(|e| {
+        eprintln!("Error: {}", e);
+        std::process::exit(1);
+    });
 
-    let input = receive_input_from_user();
-    eprint!("Input: {}\n", input);
-
+    let input = match receive_multiline_input_from_user() {
+        Ok(input) => input,
+        Err(e) => {
+            eprintln!("Error: {}", e);
+            std::process::exit(1);
+        }
+    };
+    println!("\nInput: \n{}", input);
     let result = transform::transform(input, flag);
-    eprintln!("Transformed result: {}", result);
+    println!("\nTransformed result: \n{}", result);
 }
