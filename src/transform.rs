7bb54685b79b2ce4156ef6b41de32f54a1af04e8
MrDataPsycho <mr.data.psycho@gmail.com>
2023-10-30 22:35:28 +0100

    Adding multi-threading

--- src/transform.rs
+++ src/transform.rs
@@ -1,9 +1,8 @@
 use crate::flag::Flag;
 use csv;
 use slug::slugify;
-use std::error::Error;
 
-pub fn transform(input: String, flag: Flag) -> Result<String, Box<dyn Error>> {
+pub fn transform(input: String, flag: Flag) -> Result<String, Box<dyn std::error::Error + Send>> {
     match flag {
         Flag::Lowercase => Ok(input.to_lowercase()),
         Flag::Uppercase => Ok(input.to_uppercase()),
@@ -13,15 +12,15 @@ pub fn transform(input: String, flag: Flag) -> Result<String, Box<dyn Error>> {
     }
 }
 
-fn csv_as_str(input: String) -> Result<String, Box<dyn Error>> {
+fn csv_as_str(input: String) -> Result<String, Box<dyn std::error::Error + Send>> {
     let mut content: Vec<String> = Vec::new();
     let mut reader = csv::Reader::from_reader(input.as_bytes());
-    let headers = reader.headers()?.clone();
+    let headers = reader.headers().unwrap().clone();
     let headers = headers.into_iter().collect::<Vec<&str>>();
     let headers = headers.join(",");
     content.push(headers);
     for result in reader.records() {
-        let record = result?;
+        let record = result.unwrap();
         let record = record.into_iter().collect::<Vec<&str>>();
         let record = record.join(",");
         content.push(record);
